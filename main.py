73f4280ddfeebd2002d8d76373e7e6b70f91706f
Ronen Abravanel <ronena@gmail.com>
2013-11-21 18:45:53 +0200

    nicer main

--- main.py
+++ main.py
@@ -11,27 +11,21 @@ from scipy.stats import norm
 
 
 
+
+from matplotlib import pylab
+
+
+
+
+
+
+
+
 def linear(x,a,b):
     return a*x+b
 
-
-polymer_num = 500
-
-from matplotlib import pylab
-D = 3
-L = 300
-self_avoid = True
-
-
-
-poly = Polymer(D, L, self_avoid)
-data = []
-
 collect_mu_N = []
-
 def draw_distribution(D,L,self_avoid, data_per_N):
-
-
 
     #PDF stuff
     pylab.figure()
@@ -48,55 +42,76 @@ def draw_distribution(D,L,self_avoid, data_per_N):
     pylab.legend()
     pylab.savefig("histogram_D=%d_N=%d_self_avoid=%d.pdf" % (D, N, self_avoid))
 
+
     global collect_mu_N
     collect_mu_N.append(param)
 
+def simulate_sequence(D, L, self_avoid, iterations):
 
-for N in range(50,500,50):
-    print N
-    data_per_N = []
-    bad_number = 0
-    for i in range(polymer_num):
-        (last, count, grid) = poly.create_polymer(N)
-        dist = linalg.norm(last - poly.start_point)
-        #print dist, count, last
-        if count == N:
-            data_per_N.append(dist)
-        else:
-            bad_number += 1
-        if i == 10 and D == 2:
-            pylab.figure()
-            pylab.imshow(grid, cmap=pylab.get_cmap("binary"))
-            pylab.savefig("realization_N=%d_self_avoid=%d" % (N, self_avoid))
+    poly = Polymer(D, L, self_avoid)
+    data = []
 
-    bad_ratio = 1.0*bad_number/polymer_num
-    if bad_ratio > 0.02:
-        print "To many bads with N=%d. Bad ratio %.3f" % (N, bad_ratio)
-    data_per_N = array(data_per_N)
+    for N in range(50,500,50):
+        print N
+        data_per_N = []
+        bad_number = 0
+        for i in range(iterations):
+            (last, count, grid) = poly.create_polymer(N)
+            dist = linalg.norm(last - poly.start_point)
+            #print dist, count, last
+            if count == N:
+                data_per_N.append(dist)
+            else:
+                bad_number += 1
+            if i == 10 and D == 2:
+                pylab.figure()
+                pylab.imshow(grid, cmap=pylab.get_cmap("binary"))
+                pylab.savefig("realization_N=%d_self_avoid=%d" % (N, self_avoid))
 
-    draw_distribution(D,L,self_avoid, data_per_N)
+        bad_ratio = 1.0*bad_number/iterations
+        if bad_ratio > 0.02:
+            print "To many bads with N=%d. Bad ratio %.3f" % (N, bad_ratio)
+        data_per_N = array(data_per_N)
+
+        draw_distribution(D,L,self_avoid, data_per_N)
 
 
-    R2 = (data_per_N**2).mean()
-    data.append((R2, N))
+        R2 = (data_per_N**2).mean()
+        data.append((R2, N))
 
-a_data = array(data)
-x = log(a_data.T[1])
-y = log(a_data.T[0])
+        return data
 
-popt, pcov = scipy.optimize.curve_fit(linear, x,y)
+def fit_result(data, D, self_avoid):
+    a_data = array(data)
+    x = log(a_data.T[1])
+    y = log(a_data.T[0])
 
-extrapolated_y = linear(x,popt[0],popt[1])
+    popt, pcov = scipy.optimize.curve_fit(linear, x,y)
 
-pylab.figure()
+    extrapolated_y = linear(x,popt[0],popt[1])
 
-pylab.plot(x,y, "o", label="data")
-pylab.plot(x,extrapolated_y,"-", label= "fit, %.3f  * x + %.3f" % tuple(popt))
+    pylab.figure()
 
-pylab.legend(loc="best")
+    pylab.plot(x,y, "o", label="data")
+    pylab.plot(x,extrapolated_y,"-", label= "fit, %.3f  * x + %.3f" % tuple(popt))
 
-pylab.xlabel(r"$\log \left\langle R^{2}\right\rangle$")
-pylab.ylabel(r"$\log N$")
-pylab.savefig("N_fit_D=%d_self_avoid=%d.pdf" % (D, self_avoid))
+    pylab.legend(loc="best")
 
+    pylab.xlabel(r"$\log \left\langle R^{2}\right\rangle$")
+    pylab.ylabel(r"$\log N$")
+    pylab.savefig("N_fit_D=%d_self_avoid=%d.pdf" % (D, self_avoid))
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description="Process in directory")
+    parser.add_argument("-D", type=int, nargs='?', default=2)
+    parser.add_argument("-L", type=int, nargs='?', default=500)
+    parser.add_argument("--self_avoid", type=bool, nargs='?', default=False)
+    parser.add_argument("--iterations", type=bool, nargs='?', default=500)
+
+    args = parser.parse_args()
+
+    data = simulate_sequence(args.D, args.L, args.self_avoid ,args.iterations)
+    fit_result(data, args.D, args.self_avoid)
 
